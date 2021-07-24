13b0342bdd86db64984db358546a4eb59add1826
Qs-F <qsf@de-liker.com>
2021-07-25 04:37:10 +0900

    Add tsx hooks example

--- src/example.tsx
+++ src/example.tsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
+import { useState, VFC } from 'react'
 import { INITIAL_VALUE } from '/value'
 
 export const useExample = (): {
   state: number
   increment: () => void
+  Component: VFC
 } => {
   const [state, setState] = useState(INITIAL_VALUE)
   const increment = () => setState((prev) => prev + 1)
-  return { state, increment }
+  const Component: VFC = () => <div>Hello</div>
+  return { state, increment, Component }
 }
